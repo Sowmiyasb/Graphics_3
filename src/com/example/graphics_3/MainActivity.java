2bfe8cf20a20e1c9e36c77e4f4dcd08c30f89fa1
ITLAB <ITLAB@IT>
2017-02-07 14:27:17 +0530

    2

--- src/com/example/graphics_3/MainActivity.java
+++ src/com/example/graphics_3/MainActivity.java
@@ -1,35 +1,46 @@
 package com.example.graphics_3;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	DemoView demoview;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		demoview = new DemoView(this);
+		setContentView(demoview);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private class DemoView extends View {
+		public DemoView(Context context) {
+			super(context);
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+			// custom drawing code here
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+			// make the entire canvas white
+			paint.setColor(Color.WHITE);
+			canvas.drawPaint(paint);
+			
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
